6a6b1436858989d38d95173f78acef9e81c32f96
Assafz1 <assafzfani@gmail.com>
2019-11-20 08:19:43 +0200

    fixing duplicate keys bug

--- app/src/main/java/zfani/assaf/orderscalculator/MainActivity.kt
+++ app/src/main/java/zfani/assaf/orderscalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package zfani.assaf.orderscalculator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.math.BigDecimal
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.util.*
@@ -14,22 +15,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val list1 = listOf(
-            OrdersAnalyzer.OrderLine(1, "Product1", 1, 1.0),
-            OrdersAnalyzer.OrderLine(2, "Product2", 2, 2.0),
-            OrdersAnalyzer.OrderLine(3, "Product3", 3, 3.0)
+            OrdersAnalyzer.OrderLine(1, "Product1", 1, BigDecimal(1.0)),
+            OrdersAnalyzer.OrderLine(2, "Product2", 2, BigDecimal(2.0)),
+            OrdersAnalyzer.OrderLine(3, "Product3", 3, BigDecimal(3.0))
         )
 
         val order1 = OrdersAnalyzer.Order(1, LocalDateTime.of(2019, 10, 27, 12, 0), list1)
 
         val list2 = listOf(
-            OrdersAnalyzer.OrderLine(4, "Product4", 4, 4.0),
-            OrdersAnalyzer.OrderLine(5, "Product5", 5, 5.0),
-            OrdersAnalyzer.OrderLine(6, "Product6", 6, 6.0)
+            OrdersAnalyzer.OrderLine(4, "Product4", 4, BigDecimal(4.0)),
+            OrdersAnalyzer.OrderLine(5, "Product5", 5, BigDecimal(5.0)),
+            OrdersAnalyzer.OrderLine(6, "Product6", 6, BigDecimal(6.0))
         )
 
         val order2 = OrdersAnalyzer.Order(2, LocalDateTime.now().minusDays(1), list2)
 
-        tvText.setText(OrdersAnalyzer().totalDailySales(listOf(order1, order2)).toString())
+        tvText.text = OrdersAnalyzer().totalDailySales(listOf(order1, order2)).toString()
     }
 }
 
@@ -51,12 +52,15 @@ class OrdersAnalyzer {
         val productId: Int,
         val name: String,
         val quantity: Int,
-        val unitPrice: Double
+        val unitPrice: BigDecimal
     )
 
     fun totalDailySales(orders: List<Order>): Map<DayOfWeek, Int> {
         for (order in orders) {
-            map.put(order.creationDate.dayOfWeek, getOrderLineQuantitySum(order.orderLines))
+            val key = order.creationDate.dayOfWeek
+            val value = getOrderLineQuantitySum(order.orderLines)
+            if (map.containsKey(key)) map[key] = map[key]!!.plus(value)
+            else map[key] = value
         }
         return map
     }
